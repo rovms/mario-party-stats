585d73d3e2467196559cc2999bb83b35f7f202ff
Roman Bucher <rbucher@deloitte.dk>
2022-12-30 11:59:47 +0100

    connect to correct db

--- server/db/index.js
+++ server/db/index.js
@@ -1,28 +1,27 @@
 const mongoose = require("mongoose");
 
-const CONFIG = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-};
-
 require("dotenv").config();
 
 const DB_URL = process.env.DB_URL;
+const CONFIG = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	dbName: process.env.DB_NAME,
+};
 
 const initConnection = function (callback) {
-  mongoose.connect(DB_URL, CONFIG);
-  const db = mongoose.connection;
-
-  db.on("error", function (err) {
-    console.error("Failed to connect to database");
-    process.exit(1);
-  });
-
-  db.once("open", function () {
-    console.log("DB connected.");
-    callback();
-  });
+	mongoose
+		.connect(DB_URL, CONFIG)
+		.then(() => {
+			console.log("DB Name: " + mongoose.connection.name);
+			callback();
+		})
+		.catch((err) => {
+			console.error("Failed to connect to database");
+			console.error(err);
+			process.exit(1);
+		});
 };
 
 module.exports = initConnection;
