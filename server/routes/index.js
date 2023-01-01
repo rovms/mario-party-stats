9b2652eb851b4c3019d4669b9e87fc2171a24679
Roman Bucher <romanbucher.afc@gmail.com>
2023-01-01 17:54:39 +0100

    add date to payload (#4)
    Co-authored-by: Roman Bucher <rbucher@deloitte.dk>

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,27 +3,15 @@ const router = express.Router();
 const Player = require("../model/player");
 const auth = require("../middleware/auth");
 const jwt = require("jsonwebtoken");
-const initConnection = require("../db");
 
-router.get("/player", auth, async (req, res) => {
+router.post("/score", auth, async (req, res) => {
   try {
-    const players = await Player.find();
-    return res.status(200).json(players);
-  } catch (error) {
-    return res.status(500).send(error);
-  }
-});
-
-router.post("/player", auth, async (req, res) => {
-  console.log(req.body);
-  try {
-    const now = new Date();
-    let player;
-
+    const payload = req.body;
     const invalidPlayers = [];
-    for (player of req.body) {
+    let player;
+    for (player of payload.players) {
       if (player.newPoints < 0 || player.newPoints > 7) {
-        invalidPlayers.push(player);
+        invalidPlayers.push(player); 
       }
     }
     if (invalidPlayers && invalidPlayers.length > 0) {
@@ -31,17 +19,26 @@ router.post("/player", auth, async (req, res) => {
       for (let invalidPlayer of invalidPlayers) {
         errorBody.push({ name: invalidPlayer.name, newPoints: invalidPlayer.newPoints });
       }
-      console.error(req.body);
       return res.status(400).send({ message: "Invalid points amount.", players: errorBody });
     }
-    for (player of req.body) {
+    const date = payload.date || new Date();
+    for (player of payload.players) {
       const p = await Player.findById(player.id);
-      p.scores.push({ amount: player.newPoints, date: now });
+      p.scores.push({ amount: player.newPoints, date: date });
       await p.save();
     }
     return res.status(200).json("Ok");
   } catch (error) {
     console.error(error);
+    return res.status(500).send(error);
+  }
+});
+
+router.get("/score", auth, async (req, res) => {
+  try {
+    const players = await Player.find();
+    return res.status(200).json(players);
+  } catch (error) {
     return res.status(500).send(error);
   }
 });
